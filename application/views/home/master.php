3bf3a85853f89f68758b186e2b4220cb035881f8
hoanglong1996 <42330075+hoanglong1996@users.noreply.github.com>
2018-08-14 13:51:27 +0700

    long

--- application/views/home/master.php
+++ application/views/home/master.php
@@ -17,10 +17,16 @@
         <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
       <![endif]-->
 
+       <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 
       <link rel='stylesheet' type='text/css' href='<?php echo base_url()?>public/css/app.min.css' />
       <!-- <link rel='shortcut icon' href='/Data/Sites/1/skins/default/favicon.ico' /> -->
       <script src="<?php echo base_url()?>public/js/jquery.min.js" type="text/javascript" ></script>
+
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+      <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>public/datetimepicker-master/jquery.datetimepicker.css"/ >
+      
+      <script src="<?php echo base_url()?>public/datetimepicker-master/build/jquery.datetimepicker.full.min.js"></script>
 
     </head>
     <body id="ctl00_Body" class="canhcam vi-vn">
@@ -99,6 +105,11 @@ Sys.WebForms.PageRequestManager._initialize('ctl00$ScriptManager1', 'aspnetForm'
  <div class="site-pusher">
   <?php echo isset($header)?$header:'' ?>
 
+<main class="main">
+ <div class="container">
+
+
+
   <div id="ctl00_divAlt1" class="altcontent1 cmszone">
 
 
@@ -112,14 +123,20 @@ Sys.WebForms.PageRequestManager._initialize('ctl00$ScriptManager1', 'aspnetForm'
 
     
   </ol></div></div></div>
-
+  <div style="float: right; ">
+   <button type="button" class="btn" style="background-color: white" data-toggle="modal" data-target="#myModal"><i class="fa fa-user fa-lg" style="color: #f58220;"></i>
+    <strong>Đăng Nhập</strong>
+  </button>
 </div>
-<main class="main">
- <div class="container">
+</di  v>
+
+
+
   <div class="row">
 <div id="ctl00_divLeft" class="col-left col-lg-3 cmszone">
-
+<div style="margin-top: 50px;">
    <?php echo isset($menu)?$menu:'' ?>
+ </div>
  </div>
 
    <div id="ctl00_divCenter" class="col-main col-lg-9">
@@ -219,5 +236,183 @@ document.getElementById('ctl00_mdl141_ctl00_subscribe1_regexEmail').dispose = fu
 //]]>
 </script>
 </form>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" style="width: 60%" role="document">
+    <div class="modal-content">
+      
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-6" style="padding-left: 30px; font-size: 14px;text-align: justify">
+           <h3 style="color: blue;">Tham gia cùng chúng tôi
+           </h3>
+           <p>Gia nhập Talent Network của chúng tôi sẽ giúp bạn nâng cao khả năng tìm kiếm việc làm. Cho dù bạn ứng truyển một công việc nào đó hoặc đơn giản là cập nhật thông tin của mình, chúng tôi cũng luôn mong muốn được kết nối cùng bạn.
+           </p> 
+           <ul>
+            <li>Nhận thông báo việc làm mới phù hợp với mối quan tâm của bạn</li>
+            <li>Cập nhật các thông tin mới nhất vào công ty</li>
+            <li>Chia sẻ cơ hội việc làm với gia đình, bạn bè thông qua mạng xã hội hoặc email</li> 
+          </ul>
+          <strong style="float: right;">Hãy đăng ký ngay hôm nay!</strong>
+          </div>
+          <div class="col-md-6" style="padding-right: 30px;border-left: 1px solid #ddd;">
+           <h3 style="color: blue;">Đăng Nhập</h3>
+           <br>
+          <div class="form-group row">
+            <label for="staticEmail" class="col-sm-4 col-form-label">Email</label>
+            <div class="col-sm-8">
+             
+              <input class="form-control" type="text" placeholder="Default input">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Password</label>
+            <div class="col-sm-8">
+              <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+              <button type="button" class="btn-lg" style="background-color: #f58220 !important;color: white; margin-top: 15px" data-toggle="modal" data-target="#myModal">Đăng Nhập
+            </button>
+            <p style="margin-top: 10px"><strong>
+            <i class="fa fa-caret-right fa-lg"></i> Quên mật khẩu</strong>
+            </p>  
+            <p style="margin-top: -5px"><a data-toggle="modal" data-target="#myModal1"><strong>
+            <i class="fa fa-caret-right fa-lg"></i> Đăng ký tài khoản</strong></a>
+          </p>
+            </div>
+          </div>
+          
+            
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</div>
+
+
+
+<div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" style="width: 70%" role="document">
+    <div class="modal-content">
+      
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-md-5" style="padding-left: 30px; font-size: 14px;text-align: justify">
+           <h3 style="color: blue;">Tham gia cùng chúng tôi
+           </h3>
+           <p>Gia nhập Talent Network của chúng tôi sẽ giúp bạn nâng cao khả năng tìm kiếm việc làm. Cho dù bạn ứng truyển một công việc nào đó hoặc đơn giản là cập nhật thông tin của mình, chúng tôi cũng luôn mong muốn được kết nối cùng bạn.
+           </p> 
+           <ul>
+            <li>Nhận thông báo việc làm mới phù hợp với mối quan tâm của bạn</li>
+            <li>Cập nhật các thông tin mới nhất vào công ty</li>
+            <li>Chia sẻ cơ hội việc làm với gia đình, bạn bè thông qua mạng xã hội hoặc email</li> 
+          </ul>
+          <strong style="float: right;">Hãy đăng ký ngay hôm nay!</strong>
+          </div>
+          <div class="col-md-7" style="padding-right: 30px;border-left: 1px solid #ddd;">
+           <h3 style="color: blue;">Đăng ký tài khoản</h3>
+           
+          <div class="form-group row">
+            <label for="staticEmail" class="col-sm-4 col-form-label">Email</label>
+            <div class="col-sm-8">
+           
+              <input class="form-control" type="text" placeholder="Default input">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Số CMND/CCCD</label>
+            <div class="col-sm-8">
+              <input class="form-control" type="text" placeholder="Default input">
+              
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Mật khẩu</label>
+            <div class="col-sm-8">
+              <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Xác nhận mật khẩu</label>
+            <div class="col-sm-8">
+               <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Tên</label>
+            <div class="col-sm-8">
+              <input class="form-control" type="text" placeholder="Default input">
+              
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Họ và tên đệm</label>
+            <div class="col-sm-8">
+              <input class="form-control" type="text" placeholder="Default input">
+              
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Giới tính</label>
+            <div class="col-sm-8">
+              <label class="radio-inline">
+                <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> Nam
+              </label>
+              <label class="radio-inline">
+                <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> Nữ
+              </label>
+              
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Ngày Sinh</label>
+            <div class="col-sm-8">
+              <input class="form-control" type="text" id="ngaysinh" placeholder="Default input">
+              
+            </div>
+          </div>
+          <div class="form-group row">
+            <label for="inputPassword" class="col-sm-4 col-form-label">Vị trí mong muốn</label>
+            <div class="col-sm-8">
+              <textarea class="form-control" rows="2" style="resize: none;""></textarea>
+              <button type="button" class="btn" data-toggle="modal" data-target="#myModal2" style="background-color: #f58220 !important;color: white; margin-top: 15px" data-toggle="modal" data-target="#myModal">Đăng Ký
+            </button>
+            </div>
+          </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" style="width: 70%" role="document">
+    <div class="modal-content">
+      <h3 style="text-align: center;color:blue;padding-left: 20px;padding-right: 20px;padding-top: 30px"><strong>Đất Xanh Group chào mừng bạn dã tham gia Talent Network</strong></h3>
+      <p style="padding-left:20px;padding-right: 20px">
+        Xin chào <strong>Nam</strong>, chúc mừng bạn đã đăng ký thành công tài khoản tại hệ thống Đất Xanh Talent Network
+      </p>
+      <p style="text-align: justify; padding-left:20px;padding-right: 20px">
+        Đê tiếp tục, mới bạn <a href="#" style="color:#f58220; text-decoration: underline;">hoàn thiện hồ sơ </a>của mình, điều này giúp chúng tôi có thể đánh giá hồ sơ của bạn được thuẩn lợi hơn và sau đó bạn có thể Ứng tuyển vào các vị trí mà chúng tôi đang tìm kiếm nhân tài.
+      </p>
+      <img src="<?php echo base_url()?>public/image/banner-772x250.jpg" style="width: 100%;padding-bottom: 0px">
+      
+    </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+    $('#myModal1').on('show.bs.modal', function (event) {
+        $('#myModal').modal('toggle');
+    });
+    $('#ngaysinh').datetimepicker();
+    $('#myModal2').on('show.bs.modal', function (event) {
+        $('#myModal1').modal('toggle');
+    });
+</script>
+
 </body>
 </html>
+

1f9b809af596e89478c66fc983bcc82da07582e8
heodat234 <thanhhung23495@gmail.com>
2018-09-18 10:45:23 +0700

    hung

---
+++ application/models/Candidate_model.php
@@ -0,0 +1,65 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Login_model extends CI_Model{
+	
+	/* Gán tên bảng cần xử lý*/
+	private $table = 'operator';
+	// 
+	function __construct(){
+        parent::__construct();
+        $this->load->database();
+        $this->primaryKey = 'id';
+    } 
+    public function countTableById($table,$id)
+    {
+        $this->db->select()->where('candidateid', $id);
+        $this->db->where('hidden', 1);
+        $query = $this->db->get($table)->num_rows();
+        if ($query >0) {
+            return true;
+        }else{ return false;}
+    }
+    
+    public function selectTableById($table,$id)
+    {
+        $this->db->select()->where('candidateid', $id);
+        $this->db->where('hidden', 1);
+        $query = $this->db->get($table);
+       return $query->row_array();
+    }
+    public function selectTableByIds($table,$id)
+    {
+        $this->db->select();
+        $this->db->where('candidateid', $id);
+        $this->db->where('hidden', 1);
+        $query = $this->db->get($table);
+       return $query->result_array();
+    }
+    public function updateCandidate($id,$data)
+    {
+        $a_User =   $this->db->where('candidateid', $id)
+                            ->update('candidate',$data);
+    }
+    public function InsertData($table,$data)
+    {
+        $a_User =   $this->db->insert($table,$data);
+    }
+    public function UpdateData($table,$match,$data)
+    {
+        $a_User =   $this->db->where($match)
+                            ->update($table,$data);
+    }
+    public function DeleteData($table,$match)
+    {
+        $data = array('hidden' => 0 );
+        $a_User =   $this->db->where($match)
+                            ->update($table,$data);
+    }
+    public function Set_idcandite()
+    {
+        $this->db->select('candidateid');
+        $this->db->order_by("candidateid", "desc");
+        $this->db->limit(1);
+        $this->db->from('candidate');
+        return $this->db->get()->row_array(); 
+    }
+}
